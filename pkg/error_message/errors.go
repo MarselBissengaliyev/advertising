d33dd83034d2d2ee6a3185d83977d51a7401334c
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-10-18 18:07:11 +0600

    создал константы для разных ошибок

---
+++ pkg/error_message/errors.go
@@ -0,0 +1,11 @@
+package error_message
+
+const (
+	InvalidIdParam         = "invalid id param"
+	ErrorInitConfig        = "error initializing configs"
+	ErrorLoadEnvVars       = "error loading env variables"
+	FailedToInitDB         = "failed to initialize database"
+	RunHttpServerError     = "error occured while running http server"
+	ShutDownServerError    = "error occured on server shutting down"
+	CloseDBConnectionError = "error occured on db connection close"
+)
